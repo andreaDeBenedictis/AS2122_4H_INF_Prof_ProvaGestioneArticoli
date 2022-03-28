73dcfe28121533c4000f5d4fd848aa7c54695f55
andreaDeBenedictis <100471439+andreaDeBenedictis@users.noreply.github.com>
2022-03-28 08:39:04 +0200

    Commit Compilazione Articolo

--- AS2122_4H_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4H_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -9,15 +9,25 @@ namespace AS2122_4H_INF_Prof_ProvaGestioneArticoli
     class Articolo
     {
         // TODO: (8) aggiungere attributi privati di Articolo
-        // ...
+        int codice;
+        string descrizione;
+        string unitaMisura;
+        double prezzo;
+
 
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
-        // ...
+        public int Codice { get;}
+        public string Descrizione { get; }
+        public string UnitaMisura { get; }
+        public double Prezzo { get; }
 
         public Articolo(int codice, string descrizione, string unitaMisura, double prezzo)
         {
             // TODO: (10) impostazione costruttore classe Articolo come utilizzato da frmMain
-            // ...
+            Codice = codice;
+            Descrizione = descrizione;
+            UnitaMisura = unitaMisura;
+            Prezzo = prezzo;
         }
 
         public string Visualizzati()
--- AS2122_4H_INF_Prof_ProvaGestioneArticoli/frmArticolo.cs
+++ AS2122_4H_INF_Prof_ProvaGestioneArticoli/frmArticolo.cs
@@ -30,7 +30,7 @@ namespace AS2122_4H_INF_Prof_ProvaGestioneArticoli
         private void btnSalva_Click(object sender, EventArgs e)
         {
             // TODO: (7) passaggio all' attributo/property dei dati inseriti nella frmArticoli con controllo di valorizzazione del dato
-            // ... descrizione, unitaMisura, prezzo
+            
 
             status = DialogResult.OK;
             Close();
